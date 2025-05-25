afff8218d05c437de092a56c4b7ce0343864e84e
jumartineze <jumartineze@unal.edu.co>
2025-05-25 13:05:05 -0500

    Test_01 solution

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -6,11 +6,15 @@ librerias de pandas para resolver las preguntas.
 """
 
 
+import pandas as pd
+
+
 def pregunta_01():
     """
     ¿Cuál es la cantidad de filas en la tabla `tbl0.tsv`?
 
     Rta/
     40
-
     """
+    table = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    return table.shape[0]
